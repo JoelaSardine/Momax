1fcf9784498631c90b24578cb9a704d9c8a56c48
Joe la Sardine <thibaut.poittevin@gmail.com>
2019-10-24 22:14:25 +0200

    WIP PokemonBattle layout

---
+++ Assets/Scripts/Textshadow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Textshadow : MonoBehaviour
+{
+    public string text = "text";
+    public int size = 20;
+
+    public Text realText;
+    private Text myText;
+
+    public void SetText(string text)
+    {
+        myText.text = text;
+        if (realText)
+        {
+            realText.text = text;
+        }
+    }
+    
+    private void OnValidate()
+    {
+        myText = GetComponent<Text>();
+        myText.text = text;
+        myText.fontSize = size;
+        if (realText)
+        {
+            realText.text = text;
+            realText.fontSize = size;
+        }
+    }
+}
