98b745b7d2d83fc7e01e05b070057e87eda6b667
Joe la Sardine <thibaut.poittevin@gmail.com>
2019-11-17 21:26:52 +0100

    Add fire

---
+++ Assets/Scripts/FireCollider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCollider : MonoBehaviour
+{
+    public float fireSpeed = 1.0f;
+
+    Vector2 direction = new Vector2();
+
+    public void Fire(Vector2 normalized)
+    {
+        direction = normalized;
+    }
+
+    private void Update()
+    {
+        transform.Translate(direction * Time.deltaTime * fireSpeed);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,20 @@ public class PlayerController : MonoBehaviour
     private const string INPUT_AXIS_HORIZONTAL = "Horizontal";
     private const string INPUT_AXIS_VERTICAL = "Vertical";
     private const string PLAYER_COLLIDER_INTERACTION = "InteractionCollider";
+    private const string PLAYER_COLLIDER_FIRE = "FireCollider";
+    private const string FIRE_CONTAINER = "FireContainer";
 
     private new Rigidbody2D rigidbody;
     private Animator animator;
     private Vector2 animatorParams = new Vector2();
 
     private PlayerInteractionCollider interactionCollider;
+    private Transform fireContainer;
+    private FireCollider fireCollider;
+    private List<FireCollider> fires = new List<FireCollider>();
 
     public float speed = 25.0f;
     public float interactionRange = 1.0f;
-
 
     void Awake()
     {
@@ -25,6 +29,9 @@ public class PlayerController : MonoBehaviour
 
         interactionCollider = transform.Find(PLAYER_COLLIDER_INTERACTION).GetComponent<PlayerInteractionCollider>();
         interactionCollider.Init(OnInteractionEnter, OnInteractionExit, OnInteractionStay);
+
+        fireContainer = GameObject.Find(FIRE_CONTAINER).transform;
+        fireCollider = transform.Find(PLAYER_COLLIDER_FIRE).GetComponent<FireCollider>();
     }
 
     void Update()
@@ -45,6 +52,18 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("Speed", rigidbody.velocity.magnitude);
 
         interactionCollider.transform.localPosition = new Vector3(0, 0.5f, 0) + (Vector3)animatorParams.normalized * interactionRange;
+
+        if (Input.GetButtonDown("Fire"))
+        {
+            Fire(); 
+        }
+    }
+
+    private void Fire()
+    {
+        var newFire = Instantiate(fireCollider.gameObject, fireContainer, true).GetComponent<FireCollider>();
+        fires.Add(newFire);
+        newFire.Fire(animatorParams.normalized);
     }
 
     private void OnInteractionEnter(Collider2D collision)
